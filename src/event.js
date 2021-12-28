a469fde5266119fddf1f88892839cb3662e9cd0e
ppotatoG <thgus2270@gmail.com>
2021-12-28 23:10:09 +0900

    Add - Delete list

--- src/event.js
+++ src/event.js
@@ -19,6 +19,25 @@ const addList = (item) => {
     
     let parentEl = document.querySelector(`.${className}`);
   
+    // list > inner > Done button 
+    const doneButton = document.createElement('button');
+    doneButton.className = `${className}__button`
+    doneButton.innerText = item.done ? '미완료' : '완료';
+    doneButton.dataset.done = item.done;
+    
+    // list > inner > delBbutton 
+    const delButton = document.createElement('button');
+    delButton.className = `${className}__button`
+    delButton.innerText = '삭제'
+    delButton.dataset.delete = true;
+
+    // list > inner
+    const inner = document.createElement('div');
+    inner.className = `${className}__inner`
+
+    inner.appendChild(doneButton);
+    inner.appendChild(delButton);
+
     // list > i
     const i = document.createElement('i');
     i.className = `fas fa-check ${className}__ico`
@@ -27,20 +46,13 @@ const addList = (item) => {
     const p = document.createElement('p');
     p.className = `${className}__text`
     p.innerText = item.title;
-  
-    // list > button
-    const button = document.createElement('button');
-    button.className = `${className}__button`
-    button.innerText = item.done ? '완료' : '미완료';
-    button.dataset.done = item.done;
-  
-    
+
     // list
     const li = document.createElement('li');
     li.className = `${className}__list`
     li.appendChild(i);
     li.appendChild(p);
-    li.appendChild(button);
+    li.appendChild(inner);
   
     parentEl.appendChild(li);
 }
@@ -61,57 +73,68 @@ window.addEventListener('click', (e) => {
 
     // 타겟이 버튼일 떄
     if(thisBtn.nodeName === 'BUTTON') {
+        // 버튼이 눌린 앨리먼트의 타이틀
+        const title = thisBtn.parentNode.previousSibling.innerText;
+            
+        // title 갖고있는 배열 내 객체
+        const item = todoItems.find(val => val.title === title);
+
+        // title 갖고있는 배열 내 객체의 인덱스
+        const index = todoItems.findIndex(val => val.title === title);
+
+        console.dir(item);
+        console.log(thisBtn.dataset);
+        console.dir(thisBtn.dataset);
+
+        // removeChild(item);
+        const itemEl = thisBtn.parentNode.parentNode;
+        const itemParent = itemEl.parentNode;
+
 
         // done을 변경하는 버튼일 때
-        if(thisBtn.dataset.done) {
-            // 버튼이 눌린 앨리먼트의 타이틀
-            const title = thisBtn.previousSibling.innerText;
-    
-            // title 갖고있는 배열 내 객체
-            const item = todoItems.find(val => val.title === title);
-
-            // title 갖고있는 배열 내 객체의 인덱스
-            const index = todoItems.findIndex(val => val.title === title);
-    
+        if(thisBtn.dataset.done) {    
             // item의 done을 변경
             item.done = item.done === true ? false : true;
-    
-            // removeChild(item);
-            const itemParent = thisBtn.parentNode.parentNode;
-            const itemEl = thisBtn.parentNode;
-    
-            // 리스트 삭제, 배열에서도 삭제 필요
+
+            // 리스트 삭제
             itemParent.removeChild(itemEl);
-            // delList(index);
     
             // 리스트 추가
             addList(item);
         }
 
         // 리스트 추가
-        if(thisBtn.dataset.addlist) {
+        else if(thisBtn.dataset.addlist) {
             const input = document.querySelector('#todoList');
 
+            // todoItems 배열에 item 추가
             todoItems.push({
                 id: todoItems.length, 
                 title: input.value, 
                 done: false
             })
 
+            // 도큐먼트에 item 추가
             addList(todoItems[todoItems.length - 1]);
 
+            // 입력받은 인풋에 값 삭제
             input.value = '';
         }
 
+        // 리스트 삭제
+        else if(thisBtn.dataset.delete) {
+            console.log(todoItems)
+            itemParent.removeChild(itemEl);
+            todoItems.splice(index, 1);
+        }
+
         // 모달 열기
-        if(thisBtn.dataset.modalopen) {
-            console.log(thisBtn);
+        else if(thisBtn.dataset.modalopen) {
             document.querySelector('.wrap_box-modal').style.display = 'block';
         }
 
         // 모달 닫기
-        if(thisBtn.dataset.modalclose) {
-            console.log(thisBtn);
+        else if(thisBtn.dataset.modalclose) {
             document.querySelector('.wrap_box-modal').style.display = 'none';
         }
     }
